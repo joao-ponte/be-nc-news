ec80c1e971797e5a5f39090915a6316ca587ad6e
Joao Ponte <joao_ponte@msn.com>
2024-05-28 11:59:05 +0100

    Add to app.js endpoint to /api and res

--- app/app.js
+++ app/app.js
@@ -6,6 +6,11 @@ app.use(express.json())
 
 app.get('/api/topics', getTopics)
 
+const endpoints = require('../endpoints.json')
+app.get('/api', (res) => {
+    res.status(200).send(endpoints)
+})
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: 'Internal Error' })
 })
