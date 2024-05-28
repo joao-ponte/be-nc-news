d0df707011f249ffee6ef13ec49665fa881b246b
Joao Ponte <joao_ponte@msn.com>
2024-05-28 11:33:35 +0100

    Remove unnecessary code.

--- app/Controllers/topicController.js
+++ app/Controllers/topicController.js
@@ -3,7 +3,6 @@ const { fetchTopics } = require('../Model/topicModel.js')
 exports.getTopics = async (req, res, next) => {
   try {
     const topics = await fetchTopics()
-    console.log(topics)
     res.status(200).send(topics)
   } catch (error) {
     next(error)
