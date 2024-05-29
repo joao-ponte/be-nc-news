6907b9c95e4c288691292045b8b6745ab48cb2ff
Joao Ponte <joao_ponte@msn.com>
2024-05-29 11:38:16 +0100

    Add endpoint to add.js

--- app/app.js
+++ app/app.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const { getTopics } = require('./Controllers/topicController')
-const { getArticleByID, getAllArticles } = require('./Controllers/articleController')
+const {
+  getArticleByID,
+  getAllArticles,
+} = require('./Controllers/articleController')
+const { getCommentsByArticleID } = require('./Controllers/commentController')
 const app = express()
 
 app.get('/api/topics', getTopics)
 app.get('/api/articles', getAllArticles)
 app.get('/api/articles/:article_id', getArticleByID)
+app.get('/api/articles/:article_id/comments', getCommentsByArticleID)
 
 const endpoints = require('../endpoints.json')
 app.get('/api', (req, res) => {
   res.status(200).send(endpoints)
 })
 
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Invalid path' })
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ message: err.message })
+  } else {
+    res.status(500).send({ message: 'Internal Server Error' })
+  }
 })
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: 'Internal Server Error' })
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Invalid path' })
 })
 
 module.exports = app
