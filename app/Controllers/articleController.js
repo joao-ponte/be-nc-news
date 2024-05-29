b5a6dec7c714942496d7e0df1318d016d8a1c022
Joao Ponte <joao_ponte@msn.com>
2024-05-29 16:01:27 +0100

    Add patchArticleVotes function.

--- app/Controllers/articleController.js
+++ app/Controllers/articleController.js
@@ -1,4 +1,8 @@
-const { fetchArticleByID, fetchAllArticles } = require('../Model/articleModel')
+const {
+  fetchArticleByID,
+  fetchAllArticles,
+  updateArticleVotes,
+} = require('../Model/articleModel')
 const { checkExists } = require('../Model/utilsModel')
 
 exports.getArticleByID = async (req, res, next) => {
@@ -23,6 +27,27 @@ exports.getAllArticles = async (req, res, next) => {
   try {
     const articles = await fetchAllArticles()
     res.status(200).send(articles)
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.patchArticleVotes = async (req, res, next) => {
+  try {
+    const { article_id } = req.params
+    const { inc_votes } = req.body
+
+    if (isNaN(article_id)) {
+      return res.status(400).send({ message: 'Bad request.' })
+    }
+
+    if (typeof inc_votes !== 'number') {
+      return res.status(400).send({ message: 'Invalid data type.' })
+    }
+
+    await checkExists('articles', 'article_id', article_id)
+    const updatedArticle = await updateArticleVotes(article_id, inc_votes)
+    res.status(200).send(updatedArticle)
   } catch (error) {
     next(error)
   }
