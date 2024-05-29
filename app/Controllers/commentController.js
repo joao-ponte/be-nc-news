4f8752a74a9e34fe5fc9787b32d405c5f7203f7b
Joao Ponte <joao_ponte@msn.com>
2024-05-29 13:54:18 +0100

    Create addCommentToArticle in the controller

--- app/Controllers/commentController.js
+++ app/Controllers/commentController.js
@@ -1,4 +1,7 @@
-const { fetchCommentsByArticleID } = require('../Model/commentModel')
+const {
+  fetchCommentsByArticleID,
+  addComment,
+} = require('../Model/commentModel')
 const { checkExists } = require('../Model/utilsModel')
 
 exports.getCommentsByArticleID = async (req, res, next) => {
@@ -16,5 +19,20 @@ exports.getCommentsByArticleID = async (req, res, next) => {
     } else {
       return res.status(500).send({ message: 'Internal Server Error' })
     }
+  }
+}
+
+exports.addCommentToArticle = async (req, res, next) => {
+  try {
+    const { article_id } = req.params
+    const { username, body } = req.body
+
+    if (!username || !body) {
+      return res.status(400).res({ message: 'Username and body are required.' })
+    }
+    const newComment = await addComment(article_id, username, body)
+    res.status(201).res(newComment)
+  } catch (error) {
+    next(error)
   }
 }
